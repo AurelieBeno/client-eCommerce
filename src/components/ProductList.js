3a29dad4d1161db61058a09214083443aade97e2
Raja Prahara <raja.ardri@gmail.com>
2019-03-04 17:41:47 +0100

    commented filters

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -8,6 +8,10 @@ function getProductAdress(product) {
   return `/product/${product._id}`;
 }
 
+function getProductGender(product) {
+  return `/product/${product.gender}`;
+}
+
 class ProductList extends Component {
   render() {
     const { productArray } = this.props;
@@ -16,48 +20,92 @@ class ProductList extends Component {
     return (
       <section className="product-list-container">
         <h2>All Product </h2>
-        <div className="nav-productList">
+
+        {/*   <div class="dropdown">
+         <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Gender
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#!">
+              <div>
+                <ul className="ul-container ">
+                  {productArray.map(oneProduct => {
+                    console.log(productArray);
+                    return (
+                      <li className="list-container col-3">
+                        <div className="row img">
+                          <Link to={getProductGender(oneProduct)}>
+                            <img
+                              className="list-img"
+                              src={oneProduct.baseImageUrl}
+                              alt="Item "
+                            />
+                          </Link>
+                        </div>
+                        <div className="row name">
+                          {oneProduct.name}
+                        </div>
+
+                        <div className="price">
+                          <b>{oneProduct.price} $</b>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              Men
+            </a>
+            <a class="dropdown-item" href="#!">
+              Women
+            </a>
+          </div>
+        </div> */}
+
+        {/* <div className="nav-productList">
           <ul className="nav-ul-container">
             <li>
-              <button className="btnList">Gamme de prix</button>
+              <button className="btnList">Gender</button>
             </li>
             <li>
-              <button className="btnList">Color</button>
-            </li>
-            <li>
-              <button className="btnList">Brand</button>
+              <button className="btnList">Colour</button>
             </li>
           </ul>
-        </div>
+        </div> */}
 
-        <div>
-          <ul className="ul-container ">
-            {productArray.map(oneProduct => {
-              console.log(productArray);
-              return (
-                <li className="list-container col-3">
-                  <div className="row img">
-                    <Link to={getProductAdress(oneProduct)}>
-                      <img
-                        className="list-img"
-                        src={oneProduct.baseImageUrl}
-                        alt="Item "
-                      />
-                    </Link>
-                  </div>
-                  <div className="row name">
-                    {oneProduct.name}
-                    {/* <span> {oneProduct.colour} </span> */}
-                  </div>
+        <ul className="ul-container ">
+          {productArray.map(oneProduct => {
+            console.log(productArray);
+            return (
+              <li className="list-container col-3">
+                <div className="row img">
+                  <Link to={getProductAdress(oneProduct)}>
+                    <img
+                      className="list-img"
+                      src={oneProduct.baseImageUrl}
+                      alt="Item "
+                    />
+                  </Link>
+                </div>
+                <div className="row name">
+                  {oneProduct.name}
+                  {/* <span> {oneProduct.colour} </span> */}
+                </div>
 
-                  <div className="price">
-                    <b>{oneProduct.price} $</b>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+                <div className="price">
+                  <b>{oneProduct.price} $</b>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </section>
     );
   }
