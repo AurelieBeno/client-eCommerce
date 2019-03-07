a71782132a3cca0e21c1ebc5c40030903d640e25
Raja Prahara <raja.ardri@gmail.com>
2019-03-07 20:03:30 +0100

    Not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,20 +5,25 @@ import "./NotFound.css";
 class NotFound extends Component {
   render() {
     return (
-      <div class="container container-middle">
-        <h2>Sorry! The page you are looking for does not exist.</h2>
-
-        <div className="col-md-3 offset-md-3">
-          <img src="./images/NotFound.jpeg" alt="Thinking" />
-          <p>
-            We sent our team out to think which page you want to go, but if you
-            can't wait. Please click
-          </p>
-          <a href="/" className="badge badge-light">
-            Home
-          </a>
+      <section className="AfterPayement ">
+        <div class="container container-middle">
+          <div className="text-container">
+            <h2>Sorry! The page you are looking for does not exist.</h2>
+          </div>
+          <div className="col-md-3 offset-md-3">
+            <img src="./images/NotFound.jpeg" alt="Thinking" />
+            <div className="text-container">
+              <p>
+                We sent our team out to think which page you want to go, but if
+                you can't wait. Please click
+              </p>
+              <a href="/" className="badge badge-light">
+                Home
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     );
   }
 }
